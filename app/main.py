4ab8e19754239f0923f802b7801cd2a38890333c
Yan Shytyk <shytyk.develop@gmail.com>
2026-01-27 22:32:00 +0100

    Add CORS url

--- app/main.py
+++ app/main.py
@@ -24,7 +24,11 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://income-tracker-frontend-five.vercel.app",   
+        "http://localhost:3000",                            
+        "http://127.0.0.1:3000",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
